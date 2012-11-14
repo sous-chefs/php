f8f4cd2ebfc3b5685b8953cc460442b35d86d889
jtimberman <joshua@opscode.com>
2012-11-13 23:08:15 -0700

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name              "php"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and maintains php and php modules"
-version           "1.1.0"
+version           "1.1.1"
 
 depends "build-essential"
 depends "xml"
