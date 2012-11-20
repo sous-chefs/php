a2541e3d873318d5924e28c562fac71297c23c87
Brandon Burton <brandon@inatree.org>
2012-11-19 22:46:58 -0800

    Update recipes/package.rb

--- recipes/package.rb
+++ recipes/package.rb
@@ -19,7 +19,7 @@
 #
 
 pkgs = value_for_platform(
-  %w(centos redhat scientific fedora) => {
+  %w(centos redhat scientific fedora amazon) => {
     %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => %w(php53 php53-devel php53-cli php-pear),
     'default' => %w(php php-devel php-cli php-pear)
   },
