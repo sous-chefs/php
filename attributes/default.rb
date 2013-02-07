3137b9d805189c98778a5e82bfc5e25de1a035ab
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-07 15:18:56 -0800

    Use correct conf dir for debian family

--- attributes/default.rb
+++ attributes/default.rb
@@ -36,7 +36,7 @@ when "rhel", "fedora"
     default['php']['packages'] = ['php', 'php-devel', 'php-cli', 'php-pear']
   end
 when "debian"
-  default['php']['conf_dir']      = '/etc/php5/cli'
+  default['php']['conf_dir']      = '/etc/php5'
   default['php']['ext_conf_dir']  = '/etc/php5/conf.d'
   default['php']['fpm_user']      = 'www-data'
   default['php']['fpm_group']     = 'www-data'
