c4bb7a2155c7a69b0fd99bb524aff92984823b46
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-07 15:18:37 -0800

    Stringify attribute keys for consistency

--- attributes/composer.rb
+++ attributes/composer.rb
@@ -1,3 +1,3 @@
-default[:php][:composer][:install_dir] = '/opt/php-composer'
-default[:php][:composer][:version] = 'latest' 
-default[:php][:composer][:exec] = '/usr/local/bin/composer'
+default['php']['composer']['install_dir'] = '/opt/php-composer'
+default['php']['composer']['version'] = 'latest' 
+default['php']['composer']['exec'] = '/usr/local/bin/composer'
