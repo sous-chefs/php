9f5ad1ac290df1518f62ba8ff55dd4e0ed799234
jtimberman <joshua@opscode.com>
2013-06-09 18:03:41 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and maintains php and php modules"
-version           "1.2.2"
+version           "1.2.3"
 
 depends "build-essential"
 depends "xml"
