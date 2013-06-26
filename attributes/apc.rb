ebf65877f21cbcef255592c07de9989bfedc4bfc
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-06-26 14:35:34 +0200

    Update variable-name

--- attributes/apc.rb
+++ attributes/apc.rb
@@ -21,5 +21,5 @@
 
 default['php']['apc']['shm_size'] = '128M'
 default['php']['apc']['local_size'] = '128M'
-default['php']['apc']['apc_ttl'] = '7200'
+default['php']['apc']['ttl'] = '7200'
 default['php']['apc']['user_ttl'] = '7200'
