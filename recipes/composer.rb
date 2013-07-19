c7a29df36b573a286b8f4da82ab8170732cbd0e9
Veered <lucashansne@gmail.com>
2013-07-18 17:10:04 -0700

    [COOK-3479] Added Windows support.

---
+++ recipes/composer.rb
@@ -0,0 +1,73 @@
+#
+# Author::  Lucas Hansen (<lucash@opscode.com>)
+# Cookbook Name:: php
+# Recipe:: composer
+#
+# Copyright 2013, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "php"
+include_recipe "git"
+
+execute "install_composer" do
+  action :run
+  cwd Chef::Config[:file_cache_path]
+  # This line is provided in the Composer docs as a cross-platform method of installation
+  # It is actually executing a line of PHP code
+  command %{#{node['php']['bin']} -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"}
+  not_if { system "#{node['php']['composer']['bin']} --version" }
+end
+
+# Ensure that composer is on the PATH
+if platform? 'windows'
+  php_dir = node['php']['conf_dir']
+  execute "move composer.phar #{php_dir.gsub('/', '\\')}\\composer.phar" do
+    cwd Chef::Config[:file_cache_path]
+    not_if { system "#{node['php']['composer']['bin']} --version" }
+  end
+  
+  template "#{php_dir}/#{node['php']['composer']['bin']}" do
+    source "composer.bat.erb"
+  end
+else
+  execute "mv composer.phar /usr/local/bin/#{node['php']['composer']['bin']}" do
+    cwd Chef::Config[:file_cache_path]
+    not_if { system "#{node['php']['composer']['bin']} --version" }
+  end
+end
+
+# Ensure that packages installed by composer on the PATH, but ONLY DURING THE CHEF RUN!
+if platform? 'windows'
+  ENV['PATH'] += ";#{node['php']['composer']['dir'].gsub('/', '\\')}\\bin"
+else
+  ENV['PATH'] += ":#{node['php']['composer']['dir']}/bin"
+end
+
+directory node['php']['composer']['dir'] do
+  action :create
+  recursive true
+end
+
+bin = node['php']['composer']['bin']
+execute "configure_composer" do
+  action :run
+  cwd node['php']['composer']['dir']
+  command [
+           "#{bin} init --stability dev --no-interaction",
+           "#{bin} config bin-dir bin",
+           "#{bin} config vendor-dir vendor"
+          ].join(" && ")
+end
+
--- recipes/package.rb
+++ recipes/package.rb
@@ -1,9 +1,10 @@
 #
 # Author::  Seth Chisamore (<schisamo@opscode.com>)
+# Author::  Lucas Hansen (<lucash@opscode.com>)
 # Cookbook Name:: php
 # Recipe:: package
 #
-# Copyright 2011, Opscode, Inc.
+# Copyright 2013, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,16 +19,51 @@
 # limitations under the License.
 #
 
-node['php']['packages'].each do |pkg|
-  package pkg do
-    action :install
+if platform?("windows")
+  install_dir = File.expand_path(node['php']['conf_dir']).gsub('/', '\\')
+  windows_package node['php']['windows']['msi_name'] do
+    source node['php']['windows']['msi_source']
+    # source "C:/chef/cache/php-5.3.27-win32-VC9-x86.msi"
+    installer_type :msi
+
+    options %W[
+          /quiet
+          INSTALLDIR="#{install_dir}"
+          ADDLOCAL=#{node['php']['packages'].join(',')}
+    ].join(" ")
+  end
+
+  cookbook_file "#{node['php']['conf_dir']}/PEAR/go-pear.phar" do
+    source "go-pear.phar"
+  end
+
+  template "#{node['php']['conf_dir']}/pear-options" do
+    source "pear-options.erb"
+  end
+
+  execute "install-pear" do
+    cwd node['php']['conf_dir']
+    command "go-pear.bat < pear-options"
+    creates "#{node['php']['conf_dir']}/pear.bat"
+  end
+
+  ENV['PATH'] += ";#{install_dir}"
+  windows_path install_dir
+
+else
+  node['php']['packages'].each do |pkg|
+    package pkg do
+      action :install
+    end
   end
 end
 
 template "#{node['php']['conf_dir']}/php.ini" do
   source "php.ini.erb"
-  owner "root"
-  group "root"
-  mode "0644"
+  unless platform?("windows")
+    owner "root"
+    group "root"
+    mode "0644"
+  end
   variables(:directives => node['php']['directives'])
 end
