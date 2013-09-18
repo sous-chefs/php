b34e3083eafe635b6ee9fa7df24145d4a4dbf26f
Andy Thompson <me@andytson.com>
2013-09-18 18:20:42 -0400

    [COOK-2926] Use grep_for_version to detect if package is pear/pecl
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- providers/pear.rb
+++ providers/pear.rb
@@ -252,12 +252,12 @@ def pecl?
   @pecl ||= begin
     # search as a pear first since most 3rd party channels will report pears as pecls!
     search_cmd = "#{node['php']['pear']} -d preferred_state=#{can_haz(@new_resource, "preferred_state")} search#{expand_channel(can_haz(@new_resource, "channel"))} #{@new_resource.package_name}"
-    if shell_out(search_cmd).stdout.split("\n").find { |line| line =~ /^#{@new_resource.package_name}\s+#{haz_ver(@new_resource, "version")}/ }
+    unless grep_for_version(shell_out(search_cmd).stdout, @new_resource.package_name).nil?
       false
     else
       # fall back and search as a pecl
       search_cmd = "#{node['php']['pecl']} -d preferred_state=#{can_haz(@new_resource, "preferred_state")} search#{expand_channel(can_haz(@new_resource, "channel"))} #{@new_resource.package_name}"
-      if shell_out(search_cmd).stdout.split("\n").find { |line| line =~ /^#{@new_resource.package_name}\s+#{haz_ver(@new_resource, "version")}/ }
+      unless grep_for_version(shell_out(search_cmd).stdout, @new_resource.package_name).nil?
         true
       else
         raise "Package #{@new_resource.package_name} not found in either PEAR or PECL."
