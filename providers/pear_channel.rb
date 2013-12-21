9673986c7fcb47386912f6c9c85a35c019ccf184
Julian C. Dunn <jdunn@getchef.com>
2013-12-20 21:39:22 -0500

    [COOK-2909] - remove Mixlib deprecation warning
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- providers/pear_channel.rb
+++ providers/pear_channel.rb
@@ -86,7 +86,6 @@ def exists?
   begin
     shell_out!("pear channel-info #{@current_resource.channel_name}")
     true
-  rescue Chef::Exceptions::ShellCommandFailed
   rescue Mixlib::ShellOut::ShellCommandFailed
     false
   end
