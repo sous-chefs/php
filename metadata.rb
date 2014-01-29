3346d3dfb0c28f1c63fd42943c3baf4be0dfadcc
Sean OMeara <someara@opscode.com>
2014-01-28 22:03:27 -0500

    bumping to 1.3.13

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs and maintains php and php modules'
-version           '1.3.12'
+version           '1.3.13'
 
 depends 'build-essential'
 depends 'xml'
