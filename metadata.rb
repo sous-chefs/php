34e54ea580fde604396082adedef46e0b85d1ab3
Sean OMeara <someara@opscode.com>
2014-02-27 23:16:26 -0500

    Version bump to 1.4.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs and maintains php and php modules'
-version           '1.4.0'
+version           '1.4.2'
 
 depends 'build-essential'
 depends 'xml'
