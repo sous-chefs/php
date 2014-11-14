717f361c65ef034b0d126fef52e76915bff59207
David Kinzer <dtkinzer@gmail.com>
2014-11-14 10:19:19 -0500

    Fix guard.

--- recipes/recompile.rb
+++ recipes/recompile.rb
@@ -32,7 +32,7 @@ remote_file "#{Chef::Config[:file_cache_path]}/php-#{version}.tar.gz" do
   source "#{node['php']['url']}/php-#{version}.tar.gz/from/this/mirror"
   checksum node['php']['checksum']
   mode '0644'
-  creates "#{Chef::Config[:file_cache_path]}/php-#{version}"
+  not_if { ::File.directory?("#{Chef::Config[:file_cache_path]}/php-#{version}") }
 end
 
 bash 're-build php' do
