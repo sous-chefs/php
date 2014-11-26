48854c22da9edf0c2505fb1ca767f509b4105ff7
Tim Smith <tim@cozy.co>
2014-11-25 22:22:13 -0800

    Update copyrights

--- resources/pear.rb
+++ resources/pear.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: php
 # Resource:: pear_package
 #
-# Copyright:: 2011, Opscode, Inc <legal@getchef.com>
+# Copyright:: 2011-2014, Chef Software, Inc <legal@getchef.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
