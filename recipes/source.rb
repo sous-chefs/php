ecd809a23d4c8760d8d9e2b6725f64108c92038c
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2015-06-20 16:27:56 +0300

    The latest mysql cookbook does not provide recipes anymore, user mysql_client LWRP instead

--- recipes/source.rb
+++ recipes/source.rb
@@ -24,6 +24,11 @@ include_recipe 'build-essential'
 include_recipe 'xml'
 include_recipe 'yum-epel' if node['platform_family'] == 'rhel'
 
+mysql_client 'default' do
+  action :create
+  only_if configure_options =~ /mysql/
+end
+
 pkgs = value_for_platform_family(
   %w{ rhel fedora } => %w{ bzip2-devel libc-client-devel curl-devel freetype-devel gmp-devel libjpeg-devel krb5-devel libmcrypt-devel libpng-devel openssl-devel t1lib-devel mhash-devel },
   %w{ debian ubuntu } => %w{ libbz2-dev libc-client2007e-dev libcurl4-gnutls-dev libfreetype6-dev libgmp3-dev libjpeg62-dev libkrb5-dev libmcrypt-dev libpng12-dev libssl-dev libt1-dev },
