84c30c95ee66d4d9d552b4fb4a03998876402973
Restless-ET <restless86@gmail.com>
2015-08-22 11:53:04 +0100

    Updated module_imap recipe to be more consistent with other ones

--- recipes/module_imap.rb
+++ recipes/module_imap.rb
@@ -17,13 +17,13 @@
 #
 
 pkg = value_for_platform(
-  %w(centos redhat scientific fedora) => {
-    %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => "php53-imap",
-    "default" => "php-imap"
+  %w(centos redhat scientific fedora amazon oracle) => {
+    el5_range => 'php53-imap',
+    'default' => 'php-imap'
   },
-  "default" => "php5-imap"
+  'default' => 'php5-imap'
 )
 
 package pkg do
-  action :upgrade
+  action :install
 end
