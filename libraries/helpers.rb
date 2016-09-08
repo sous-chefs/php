4403bc3ff2e06d4da5ed2892937f49249958253f
Tim Smith <tsmith@chef.io>
2016-09-07 21:12:26 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: php
 # Libraries:: helpers
 #
-# Copyright 2013-2015, Chef Software, Inc.
+# Copyright 2013-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
