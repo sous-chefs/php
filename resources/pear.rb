af0457daae4fd6bfba86897519f1eaed79f1cd42
Dan Webb <dan.webb@damacus.io>
2018-02-10 16:55:06 +0000

    Tidy up not_if block, let centos-6 fail
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- resources/pear.rb
+++ resources/pear.rb
@@ -50,7 +50,7 @@ action :install do
       info_output << " version #{install_version}" if install_version && !install_version.empty?
       Chef::Log.info(info_output)
       install_package(new_resource.package_name, install_version)
-      not_if { candidate_version == current_installed_version(new_resource) }
+      not_if { versions_match }
     end
   end
 end
