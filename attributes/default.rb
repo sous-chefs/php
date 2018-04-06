09c6dffc38fb244e521114f2bbf116bd994ae92f
Tim Smith <tsmith@chef.io>
2018-04-05 20:11:26 -0400

    Add a basic php pear attribute
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,6 +26,8 @@ default['php']['pecl'] = 'pecl'
 
 default['php']['version'] = '5.6.30'
 
+default['php']['pear'] = 'pear'
+
 default['php']['url'] = 'http://us1.php.net/get'
 default['php']['checksum'] = '8bc7d93e4c840df11e3d9855dcad15c1b7134e8acf0cf3b90b932baea2d0bde2'
 default['php']['prefix_dir'] = '/usr/local'
