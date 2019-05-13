e0b37077271484e33e5345346d71f3666e185519
Dan Webb <dan.webb@damacus.io>
2019-05-13 19:57:06 +0100

    Fix linting
    - cookstyle
    - add circleCi testing

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name 'php'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
+maintainer 'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
 license 'Apache-2.0'
 description 'Installs and maintains php and php modules'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '6.1.1'
+source_url 'https://github.com/chef-cookbooks/php'
+issues_url 'https://github.com/chef-cookbooks/php/issues'
+chef_version '>= 13.0'
+version '7.0.0'
 
 depends 'build-essential', '>= 5.0'
 depends 'yum-epel'
@@ -12,11 +15,3 @@ depends 'yum-epel'
 %w(amazon centos debian fedora oracle redhat scientific suse opensuse opensuseleap ubuntu).each do |os|
   supports os
 end
-
-recipe 'php', 'Installs php'
-recipe 'php::package', 'Installs php using packages.'
-recipe 'php::source', 'Installs php from source.'
-
-source_url 'https://github.com/chef-cookbooks/php'
-issues_url 'https://github.com/chef-cookbooks/php/issues'
-chef_version '>= 12.7' if respond_to?(:chef_version)
