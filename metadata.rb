9819d80075e9f812697532442dc69c7888890288
Sufian Abu-Rab <s.aburab@gmail.com>
2019-09-21 21:28:19 +0200

    increase because of bug fix

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/sous-chefs/php'
 issues_url 'https://github.com/sous-chefs/php/issues'
 chef_version '>= 14.0'
-version '7.0.0'
+version '7.0.1'
 
 depends 'yum-epel'
 
