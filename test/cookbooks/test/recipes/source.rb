b22014db7a5b437de7def5461b3bd1f4d5090800
Tim Smith <tsmith84@gmail.com>
2020-06-30 11:42:08 -0700

    Use a different mirror in testing since GH Actions is blocked
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/source.rb
+++ test/cookbooks/test/recipes/source.rb
@@ -2,5 +2,6 @@ apt_update
 
 node.default['php']['install_method'] = 'source'
 node.default['php']['pear'] = '/usr/local/bin/pear'
+node.default['php']['url'] = 'https://ftp.osuosl.org/pub/php/' # the default site blocks github actions boxes
 
 include_recipe 'php'
