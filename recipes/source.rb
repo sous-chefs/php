d9559a860b8d8a7909a0e72c525845bff9183f2a
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-07-09 17:07:48 -0400

    fix: Add guard for source builds on Debian 9
    It does not build properly, so we are dropping source install support

--- recipes/source.rb
+++ recipes/source.rb
@@ -18,6 +18,10 @@
 # limitations under the License.
 #
 
+if platform?('debian') && node['platform_version'].to_i == 9
+  Chef::Log.fatal 'Debian 9 is not supported when building from source'
+end
+
 version = node['php']['version']
 configure_options = node['php']['configure_options'].join(' ')
 ext_dir_prefix = node['php']['ext_dir'] ? "EXTENSION_DIR=#{node['php']['ext_dir']}" : ''
