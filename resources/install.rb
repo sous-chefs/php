0031b920e4d26c610a90c851049b0a1e8f3727f6
ArtofBugs <oweng@scarlet.workstation.cass.oregonstate.edu>
2023-09-13 16:39:12 -0700

    Update properties
    Signed-off-by: ArtofBugs <oweng@scarlet.workstation.cass.oregonstate.edu>

--- resources/install.rb
+++ resources/install.rb
@@ -2,16 +2,15 @@ unified_mode true
 include Php::Cookbook::Helpers
 
 property :packages, Array, default: lazy { php_installation_packages }
-property :community_package, [true, false], default: false
-property :source, [true, false], default: false
-# property :options
-# property :fpm_ini_control
+property :install_method, ['package', 'community_package', 'source'], default: lazy { php_install_method }
+property :directives, Hash, default: lazy { php_directives }
+property :options, Array, default: lazy { php_configure_options }
 
 action :install do
   package 'Install PHP Packages' do
     package_name new_resource.packages
 
-    if new_resource.community_package
+    if new_resource.install_method == 'community_package'
       if platform_family?('rhel', 'amazon')
         include_recipe 'yum-remi-chef::remi'
       elsif platform?('ubuntu')
@@ -26,95 +25,102 @@ action :install do
       end
     end
 
-    # include_recipe 'php::package'
-      # package node['php']['packages'] do
-        # options node['php']['package_options']
-      # end
-      # include_recipe 'php::ini'
-  else if new_resource.source
-    if platform?('debian') && node['platform_version'].to_i == 9
-      Chef::Log.fatal 'Debian 9 is not supported when building from source'
-    end
-    ext_dir_prefix = php_ext_dir ? "EXTENSION_DIR=#{php_ext_dir}" : ''
+    else if new_resource.install_method == 'source'
+      if platform?('debian') && node['platform_version'].to_i == 9
+        Chef::Log.fatal 'Debian 9 is not supported when building from source'
+      end
+      ext_dir_prefix = php_ext_dir ? "EXTENSION_DIR=#{php_ext_dir}" : ''
 
-    php_exists = if new_resource.src_recompile
-                   false
-                 else
-                   "$(which #{php_bin}) --version | grep #{new_resource.version}"
-                 end
-    build_essential
+      php_exists = if php_src_recompile
+                     false
+                   else
+                     "$(which #{php_bin}) --version | grep #{php_version}"
+                   end
+      build_essential
 
-    include_recipe 'yum-epel' if platform_family?('rhel', 'amazon')
+      include_recipe 'yum-epel' if platform_family?('rhel', 'amazon')
 
-    package 'Install PHP Source Dependencies' do
-      package_name new_resource.src_deps
-      action :install
-    end
+      package 'Install PHP Source Dependencies' do
+        package_name php_src_deps
+        action :install
+      end
 
-    log "php_exists == #{php_exists}"
+      log "php_exists == #{php_exists}"
 
-    remote_file "#{Chef::Config[:file_cache_path]}/php-#{new_resource.version}.tar.gz" do
-      source "#{new_resource.url}/php-#{new_resource.version}.tar.gz"
-      checksum new_resource.checksum
-      action :create_if_missing
-      not_if php_exists
-      notifies :run, 'execute[un-pack php]', :immediately
-    end
+      remote_file "#{Chef::Config[:file_cache_path]}/php-#{php_version}.tar.gz" do
+        source "#{php_url}/php-#{php_version}.tar.gz"
+        checksum php_checksum
+        action :create_if_missing
+        not_if php_exists
+        notifies :run, 'execute[un-pack php]', :immediately
+      end
 
-    execute 'un-pack php' do
-      cwd Chef::Config[:file_cache_path]
-      command "tar -zxf php-#{new_resource.version}.tar.gz"
-      creates "#{Chef::Config[:file_cache_path]}/php-#{new_resource.version}"
-      action :nothing
-    end
+      execute 'un-pack php' do
+        cwd Chef::Config[:file_cache_path]
+        command "tar -zxf php-#{php_version}.tar.gz"
+        creates "#{Chef::Config[:file_cache_path]}/php-#{php_version}"
+        action :nothing
+      end
 
-    if php_ext_dir
-      directory php_ext_dir do
+      if php_ext_dir
+        directory php_ext_dir do
+          owner 'root'
+          group 'root'
+          mode '0755'
+          recursive true
+        end
+      end
+
+      link '/usr/include/gmp.h' do
+        to '/usr/include/x86_64-linux-gpu/gmp.h'
+        only_if { platform?('ubuntu') && node['platform_version'].to_f == 16.04 }
+      end
+
+      execute 'clean build' do
+        cwd "#{Chef::Config[:file_cache_path]}/php#{php_version}"
+        command 'make clean'
+        only_if { php_src_recompile }
+      end
+
+      execute 'configure php' do
+        cwd "#{Chef::Config[:file_cache_path]}/php-#{php_version}"
+        command "#{ext_dir_prefix} ./configure #{new_resource.options.join(' ')}"
+        not_if php_exists
+      end
+
+      execute 'build and install php' do
+        cwd "#{Chef::Config[:file_cache_path]}/php-#{php_version}"
+        command "make -j #{node['cpu']['total']} && make install"
+        not_if php_exists
+      end
+
+      directory php_conf_dir do
         owner 'root'
         group 'root'
         mode '0755'
         recursive true
       end
+
+      directory php_ext_conf_dir do
+        owner 'root'
+        group 'root'
+        mode '0755'
+        recursive true
+      end
+
     end
 
-    link '/usr/include/gmp.h' do
-      to '/usr/include/x86_64-linux-gpu/gmp.h'
-      only_if { platform?('ubuntu') && node['platform_version'].to_f == 16.04 }
+    else
+      package 'Install PHP Packages' do
+        package_name new_resource.packages
+        options new_resource.options
+      end
     end
-
-    execute 'clean build' do
-      cwd "#{Chef::Config[:file_cache_path]}/php#{new_resource.version}"
-      command 'make clean'
-      only_if { new_resource.src_recompile }
-    end
-
-    execute 'configure php' do
-      cwd "#{Chef::Config[:file_cache_path]}/php-#{new_resource.version}"
-      command "#{ext_dir_prefix} ./configure #{new_resource.configure_options.join(' ')}"
-      not_if php_exists
-    end
-
-    execute 'build and install php' do
-      cwd "#{Chef::Config[:file_cache_path]}/php-#{new_resource.version}"
-      command "make -j #{node['cpu']['total']} && make install"
-      not_if php_exists
-    end
-
-    directory php_conf_dir do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      recursive true
-    end
-
-    directory php_ext_conf_dir do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      recursive true
-    end
-
-  end
+  # include_recipe 'php::package'
+    # package node['php']['packages'] do
+      # options node['php']['package_options']
+    # end
+    # include_recipe 'php::ini'
 
   php_config 'php-config' do
     action :install
