875cf6a7f4db1eb0bda40d88fb51f4901efeaf20
ArtofBugs <oweng@osuosl.org>
2023-09-15 17:55:30 -0700

    Edit properties for customization for community installs
    Signed-off-by: ArtofBugs <oweng@osuosl.org>

--- resources/ini.rb
+++ resources/ini.rb
@@ -1,11 +1,14 @@
 unified_mode true
 include Php::Cookbook::Helpers
 
-property :fpm_service, String, default: lazy { php_fpm_service }
 property :fpm_ini_control, [true, false], default: false
-property :ini_template, String, default: 'php.ini.erb'
-property :ini_cookbook, String, default: 'php'
+property :fpm_service, String, default: lazy { php_fpm_service }
+property :fpm_conf_dir, String, default: lazy { php_fpm_conf_dir }
+property :ini_template, String, default: lazy { php_ini_template }
+property :ini_cookbook, String, default: lazy { php_ini_cookbook }
 property :directives, Hash, default: {}
+property :ext_dir, String, default: lazy { php_ext_dir }
+property :conf_dir, String, default: lazy { php_conf_dir }
 
 action :install do
   if new_resource.fpm_ini_control
@@ -14,7 +17,7 @@ action :install do
       action :enable
     end
 
-    template "#{php_fpm_conf_dir}/php.ini" do
+    template "#{new_resource.fpm_conf_dir}/php.ini" do
       source new_resource.ini_template
       cookbook new_resource.ini_cookbook
       owner 'root'
@@ -23,14 +26,14 @@ action :install do
       manage_symlike_source true
       variables(
         directives: new_resource.directives,
-        php_ext_dir: php_ext_dir
+        php_ext_dir: new_resource.ext_dir
       )
       notifies :restart, "service[#{new_resource.fpm_service}]"
       not_if { new_resource.fpm_conf_dir.nil? }
     end
   end
 
-  template "#{php_conf_dir}/php.ini" do
+  template "#{new_resource.conf_dir}/php.ini" do
     source new_resource.ini_template
     cookbook new_resource.ini_cookbook
     owner 'root'
@@ -39,11 +42,7 @@ action :install do
     manage_symlink_source true
     variables(
       directives: new_resource.directives,
-      php_ext_dir: php_ext_dir
+      php_ext_dir: new_resource.ext_dir
     )
   end
-end
-
-action_class do
-  include Php::Cookbook::Helpers
 end
