e9ad69379d89b64606176d8c9fb940b6c69747d1
ArtofBugs <oweng@osuosl.org>
2023-09-29 14:22:56 -0700

    Use php_version helper instead of case statements in helpers; change php_version to use major-minor only
    Signed-off-by: ArtofBugs <oweng@osuosl.org>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,30 +2,10 @@ module Php
   module Cookbook
     module Helpers
       def php_conf_dir
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           '/etc'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/etc/php/7.3/cli'
-            else # >= 11
-              '/etc/php/7.4/cli'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/etc/php/7.2/cli'
-            when 20.04
-              '/etc/php/7.4/cli'
-            else # >= 22.04
-              '/etc/php/8.1/cli'
-            end
-          else
-            '/etc/php/7.0/cli'
-          end
+        else
+          "/etc/php/#{php_version}/cli"
         end
       end
 
@@ -38,30 +18,10 @@ module Php
       end
 
       def php_ext_conf_dir
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           '/etc/php.d'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/etc/php/7.3/mods-available'
-            else # >= 11
-              '/etc/php/7.4/mods-available'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/etc/php/7.2/mods-available'
-            when 20.04
-              '/etc/php/7.4/mods-available'
-            else # >= 22.04
-              '/etc/php/8.1/mods-available'
-            end
-          else
-            '/etc/php/7.0/mods-available'
-          end
+        else
+          "/etc/php/#{php_version}/mods-available"
         end
       end
 
@@ -70,58 +30,18 @@ module Php
       end
 
       def php_fpm_conf_dir
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           '/etc/php-fpm.d'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/etc/php/7.3/fpm'
-            else # >= 11
-              '/etc/php/7.4/fpm'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/etc/php/7.2/fpm'
-            when 20.04
-              '/etc/php/7.4/fpm'
-            else # >= 22.04
-              '/etc/php/8.1/fpm'
-            end
-          else
-            '/etc/php/7.0/fpm'
-          end
+        else
+          "/etc/php/#{php_version}/fpm"
         end
       end
 
       def php_fpm_default_conf
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           '/etc/php-fpm.d/www.conf'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/etc/php/7.3/fpm/pool.d/www.conf'
-            else # >= 11
-              '/etc/php/7.4/fpm/pool.d/www.conf'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/etc/php/7.2/fpm/pool.d/www.conf'
-            when 20.04
-              '/etc/php/7.4/fpm/pool.d/www.conf'
-            else # >= 22.04
-              '/etc/php/8.1/fpm/pool.d/www.conf'
-            end
-          else
-            '/etc/php/7.0/fpm/pool.d/www.conf'
-          end
+        else
+          "/etc/php/#{php_version}/fpm/pool.d/www.conf"
         end
       end
 
@@ -153,58 +73,18 @@ module Php
       end
 
       def php_fpm_package
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           'php-fpm'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              'php7.3-fpm'
-            else # >= 11
-              'php7.4-fpm'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              'php7.2-fpm'
-            when 20.04
-              'php7.4-fpm'
-            else # >= 22.04
-              'php8.1-fpm'
-            end
-          else
-            'php7.0-fpm'
-          end
+        else
+          "php#{php_version}-fpm"
         end
       end
 
       def php_fpm_pool_dir
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           '/etc/php-fpm.d'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/etc/php/7.3/fpm/pool.d'
-            else # >= 11
-              '/etc/php/7.4/fpm/pool.d'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/etc/php/7.2/fpm/pool.d'
-            when 20.04
-              '/etc/php/7.4/fpm/pool.d'
-            else # >= 22.04
-              '/etc/php/8.1/fpm/pool.d'
-            end
-          else
-            '/etc/php/7.0/fpm/pool.d'
-          end
+        else
+          "/etc/php/#{php_version}/fpm/pool.d"
         end
       end
 
@@ -217,58 +97,18 @@ module Php
       end
 
       def php_fpm_service
-        case node['platform_family']
-        when 'rhel', 'amazon'
+        if platform_family?('rhel', 'amazon')
           'php-fpm'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              'php7.3-fpm'
-            else # >= 11
-              'php7.4-fpm'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              'php7.2-fpm'
-            when 20.04
-              'php7.4-fpm'
-            else # >= 22.04
-              'php8.1-fpm'
-            end
-          else
-            'php7.0-fpm'
-          end
+        else
+          "php#{php_version}-fpm"
         end
       end
 
       def php_fpm_socket
-        case node['platform_family']
-        when 'rhel', 'amazon'
-          '/var/run/php7.2-fpm.sock'
-        when 'debian'
-          case node['platform']
-          when 'debian'
-            case node['platform_version'].to_i
-            when 10
-              '/var/run/php/php7.3-fpm.sock'
-            else # >= 11
-              '/var/run/php/php7.4-fpm.sock'
-            end
-          when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              '/var/run/php/php7.2-fpm.sock'
-            when 20.04
-              '/var/run/php/php7.4-fpm.sock'
-            else # >= 22.04
-              '/var/run/php/php8.1-fpm.sock'
-            end
-          else
-            '/var/run/php/php7.0-fpm.sock'
-          end
+        if platform_family?('rhel', 'amazon')
+          "/var/run/php#{php_version}-fpm.sock"
+        else
+          "/var/run/php/php#{php_version}-fpm.sock"
         end
       end
 
@@ -297,22 +137,13 @@ module Php
         # so versions are pinned here to avoid packages getting ahead of each
         # other
         when 'amazon'
-          %w(php8.2 php8.2-devel php8.2-cli php-pear)
+          ["php#{php_version}", "php#{php_version}-devel", "php#{php_version}-cli", 'php-pear']
         when 'debian'
           case node['platform']
           when 'debian'
             %w(php-cgi php php-dev php-cli php-pear)
           when 'ubuntu'
-            case node['platform_version'].to_f
-            when 18.04
-              %w(php7.2-cgi php7.2 php7.2-dev php7.2-cli php-pear)
-            when 20.04
-              %w(php7.4-cgi php7.4 php7.4-dev php7.4-cli php-pear)
-            else # >= 22.04
-              %w(php8.1-cgi php8.1 php8.1-dev php8.1-cli php-pear)
-            end
-          else
-            %w(php7.0-cgi php7.0 php7.0-dev php7.0-cli php-pear)
+            ["php#{php_version}-cgi", "php#{php_version}", "php#{php_version}-dev", "php#{php_version}-cli", 'php-pear']
           end
         end
       end
@@ -324,27 +155,27 @@ module Php
       def php_version
         case node['platform_family']
         when 'rhel', 'amazon'
-          '7.2.31'
+          '7.2'
         when 'debian'
           case node['platform']
           when 'debian'
             case node['platform_version'].to_i
             when 10
-              '7.3.19'
+              '7.3'
             else
-              '7.4.27' # >= 11
+              '7.4' # >= 11
             end
           when 'ubuntu'
             case node['platform_version'].to_f
             when 18.04
-              '7.2.31'
+              '7.2'
             when 20.04
-              '7.4.7'
+              '7.4'
             else # >= 22.04
-              '8.1.7'
+              '8.1'
             end
           else
-            '7.0.4'
+            '7.0'
           end
         end
       end
