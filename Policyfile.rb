d55fc53c353b2338c87f534719049267789f1455
Dan Webb <dan.webb@damacus.io>
2026-07-02 07:44:34 +0100

    fix: pin legacy yum-epel test dependency

--- Policyfile.rb
+++ Policyfile.rb
@@ -9,7 +9,7 @@ cookbook 'apt', git: 'https://github.com/sous-chefs/apt.git', branch: 'main'
 cookbook 'ondrej_ppa_ubuntu', git: 'https://github.com/MelonSmasher/ondrej_ppa_ubuntu.git', branch: 'master'
 cookbook 'test', path: './test/cookbooks/test'
 cookbook 'yum', git: 'https://github.com/sous-chefs/yum.git', branch: 'main'
-cookbook 'yum-epel', git: 'https://github.com/sous-chefs/yum-epel.git', branch: 'main'
+cookbook 'yum-epel', git: 'https://github.com/sous-chefs/yum-epel.git', tag: '5.0.9'
 cookbook 'yum-remi-chef', '>= 5.0.1', git: 'https://github.com/sous-chefs/yum-remi-chef.git', tag: '5.0.1'
 
 Dir.children('./test/cookbooks/test/recipes').grep(/\.rb\z/).sort.each do |recipe|
